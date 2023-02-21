5a0b208a2eaddb70b9b7cc92bdff6f1215491678
Peniel Dialu <dialundamapeniel@gmail.com>
2023-02-21 13:41:45 +0100

    eas config

---
+++ .versionrc.js
@@ -0,0 +1,78 @@
+module.exports = {
+  // expo-versioning
+  bumpFiles: [
+    {
+      filename: "package.json",
+    },
+    {
+      filename: "package-lock.json",
+    },
+    {
+      filename: "app.json",
+      updater: require.resolve("standard-version-expo"),
+    },
+    {
+      filename: "app.json",
+      updater: require.resolve("standard-version-expo/android"),
+    },
+    {
+      filename: "app.json",
+      updater: require.resolve("standard-version-expo/ios"),
+    },
+  ],
+  types: [
+    {
+      type: "feat",
+      section: "Features",
+    },
+    {
+      type: "fix",
+      section: "Bug Fixes",
+    },
+    {
+      type: "cfix",
+      section: "Dev Bug Fixes",
+      hidden: true,
+    },
+    {
+      type: "wip",
+      section: "Working in progress",
+      hidden: true,
+    },
+    {
+      type: "chore",
+      hidden: true,
+    },
+    {
+      type: "docs",
+      hidden: true,
+    },
+    {
+      type: "style",
+      hidden: true,
+    },
+    {
+      type: "refactor",
+      hidden: true,
+    },
+    {
+      type: "perf",
+      hidden: true,
+    },
+    {
+      type: "build",
+      hidden: true,
+    },
+    {
+      type: "test",
+      hidden: true,
+    },
+    {
+      type: "revert",
+      hidden: true,
+    },
+  ],
+  commitUrlFormat: "https://github.com/bilwifi/gh-action-expo/commit/{{hash}}",
+  compareUrlFormat:
+    "https://github.com/bilwifi/gh-action-expo/compare/{{previousTag}}...{{currentTag}}",
+};
